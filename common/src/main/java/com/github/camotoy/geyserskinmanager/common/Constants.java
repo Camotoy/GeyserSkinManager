4b0ecbf1021a447956b0145efbda0a8503ff103a
Camotoy <20743703+Camotoy@users.noreply.github.com>
2021-02-06 17:54:05 -0500

    Shorten plugin message channel names (untested)

--- common/src/main/java/com/github/camotoy/geyserskinmanager/common/Constants.java
+++ common/src/main/java/com/github/camotoy/geyserskinmanager/common/Constants.java
@@ -4,28 +4,28 @@ package com.github.camotoy.geyserskinmanager.common;
  * A collection of static variables used across versions.
  */
 public final class Constants {
-    public static final String BEDROCK_SKIN_UTILITY_INIT_NAME = "bedrockskinutility:init";
+    public static final String BEDROCK_SKIN_UTILITY_INIT_NAME = "bedrockskin:init";
     /**
      * The version sent and checked as the first component of a plugin message.
      * Used for future-proofing, in case the contents of a plugin message changes.
      */
     public static final int BEDROCK_SKIN_UTILITY_INIT_VERSION = 1;
 
-    public static final String CAPE_PLUGIN_MESSAGE_NAME = "geyserskinmanager:cape";
+    public static final String CAPE_PLUGIN_MESSAGE_NAME = "geyserskin:cape";
     /**
      * The version sent and checked as the first component of a plugin message.
      * Used for future-proofing, in case the contents of a plugin message changes.
      */
     public static final int CAPE_PLUGIN_MESSAGE_VERSION = 1;
 
-    public static final String INIT_PLUGIN_MESSAGE_NAME = "geyserskinmanager:init";
+    public static final String INIT_PLUGIN_MESSAGE_NAME = "geyserskin:init";
     /**
      * The version sent and checked as the first component of a plugin message.
      * Used for future-proofing, in case the contents of a plugin message changes.
      */
     public static final int INIT_PLUGIN_MESSAGE_VERSION = 1;
 
-    public static final String SKIN_PLUGIN_MESSAGE_NAME = "geyserskinmanager:skin";
+    public static final String SKIN_PLUGIN_MESSAGE_NAME = "geyserskin:skin";
     /**
      * The version sent and checked as the first component of a plugin message.
      * Used for future-proofing, in case the contents of a plugin message changes.
