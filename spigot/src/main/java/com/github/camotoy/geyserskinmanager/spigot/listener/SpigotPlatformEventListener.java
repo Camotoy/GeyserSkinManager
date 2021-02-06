43640134cb3789b08e8e522c4c7e1d5dc466b8dc
Leialoha <46801087+Leialoha@users.noreply.github.com>
2021-02-06 14:34:27 -0700

    Update SpigotPlatformEventListener.java

--- spigot/src/main/java/com/github/camotoy/geyserskinmanager/spigot/listener/SpigotPlatformEventListener.java
+++ spigot/src/main/java/com/github/camotoy/geyserskinmanager/spigot/listener/SpigotPlatformEventListener.java
@@ -83,29 +83,33 @@ public abstract class SpigotPlatformEventListener implements Listener, PluginMes
         }
     }
     
-//     public RawSkin RetrieveSkin(Player player, RawSkin skin) {
-//         PlayerEntry playerEntry = database.getPlayerEntry(player.getUniqueId());
+    public String[] RetrieveSkin(Player player, RawSkin skin) {
+        PlayerEntry playerEntry = database.getPlayerEntry(player.getUniqueId());
         
-//         SkinEntry setSkin = null;
-//         for (SkinEntry skinEntry : playerEntry.getSkinEntries()) {
-//             if (skinEntry.getBedrockSkin().equals(skin.rawData)) {
-//                 setSkin = skinEntry;
-//                 break;
-//             }
-//         }
-//         if (setSkin == null) {
-//             uploadSkin(skin, profile, player, playerEntry);
-//         } else {
-//             // We have the skin, we can go straight to applying it to the player
-//             setSkin(profile, player, setSkin);
-//         }
+        SkinEntry setSkin = null;
+        int dateAdded = 0;
+        for (SkinEntry skinEntry : playerEntry.getSkinEntries()) {
+            if (dateAdded < skinEntry.dateAdded()) {
+                dateAdded = skinEntry.dateAdded();
+                setSkin = skinEntry;
+            }
+        }
+        if (setSkin != null) {
+            return {setSkin.javaSkinValue(), setSkin.javaSkinSignature()};
+        }
         
-//         PlayerEntry playerEntry = database.getPlayerEntry(player.getUniqueId());
-
-//         SkinEntry skinEntry = new SkinEntry(skin.rawData, uploadResult.getResponse().get("value").getAsString(),
-//                     uploadResult.getResponse().get("signature").getAsString(), false);
-//     }
-
+        /*
+            Default Steve Skin, Value and Signature
+            This is Provided if setSkin is Null
+            (Just to bypass any errors)
+        */
+        
+        String value = "ewogICJ0aW1lc3RhbXAiIDogMTYxMjY0NzA0MzEwOCwKICAicHJvZmlsZUlkIiA6ICJmYmYyMmIyMjNjN2E0NzA0OGYwM2U0MzVhZGFhNGVhZiIsCiAgInByb2ZpbGVOYW1lIiA6ICJCZWFyZGVkU3RldmUiLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMWE0YWY3MTg0NTVkNGFhYjUyOGU3YTYxZjg2ZmEyNWU2YTM2OWQxNzY4ZGNiMTNmN2RmMzE5YTcxM2ViODEwYiIKICAgIH0KICB9Cn0=";
+        String signature = "Et91ODhR0JTM3jQvI3x9b0d1mdAw1O9iE8vx5M0UfaV+qiCpH0S2OjkyIAN+vRi7FK6DjIvAEng3Z6MZYa+BexicYRUJBstpPHI36jcoJMbZ/EHZcUok4bb4LRQLrYRTlMPGUZU7CwVXMCu33p60TR6tx9mSFIQFJiAbnK0Fj5+9y2toLXKn/pkntnJHR/fS6oImILHx/pTnPPLjG9ixSot4C9kmdcdTS5FKfui4kU8p0SWjwC3glqvGOslZngbReke8sduNvVUFiqRKELZnYCloAs0q6l+TQigcxwlidtV5fmbRg/kHTuHjCahaY2REYKJu0E7IWyLQh063KvhqplZLiAkhj1KsldAFDhwIAqR9M0eO9DDnUVFWv+gSlNLu23eHzKwwb2g/c3HchVyfo15qpVAgL+qzhzcrUumWCLR42ywMvzddH7L3o/cnJI5leQ6lQUb2CeVhPTTarnIXwti/Q1NO16gr/GN2an2Tpin/Ucc/zJsLVZwG0KnhWnmWUEs3sURcpEv3x02A95hMcTQbDIiZ71XvVW77i7nD9P9/DSY2WSfc2JpjGX0RKMKOBDr6tzz4ShAsJrWz+N2x3QEV7w9r/2paPgR9nnKBQeTYFkM8H43WhJjOmz9B4HR0zKn+gD/azUWmynEt+zEMZ1Lnlv3x/YHzfrkmPDmfQJY=";
+        
+        return {value, signature}
+    }
+    
     protected void uploadSkin(RawSkin skin, MinecraftProfileWrapper profile, Player player, PlayerEntry playerEntry) {
         skinUploader.uploadSkin(skin).whenComplete((uploadResult, throwable) -> {
             if (!skinUploader.checkResult(plugin.getLogger(), player.getName(), uploadResult, throwable)) {
